3df820b388c0d4850ac4f7301848046dd76425b8
jpcarreira <jpmavcarreira@gmail.com>
2013-01-14 03:43:52 +0000

    doc Utilizador.h

--- Gestao/Gestao/Utilizador.h
+++ Gestao/Gestao/Utilizador.h
@@ -1,9 +1,9 @@
 /**
-*@author Andre Silva
-*@date 08/01/2013
-*@file Utilizador.h
-*Classe responsavel por guardar a informacao acerca do utilizador.
-*/
+ * @author Andre Silva
+ * @date 08/01/2013
+ * @file Utilizador.h
+ * Classe responsavel por guardar a informacao acerca do utilizador.
+ */
 
 #ifndef Utilizador_
 #define Utilizador_
@@ -37,22 +37,24 @@ public:
 	void escreve(ostream &out) const;
 };
 
+
 /**
-*Construtor vazio.
-*/
+ * Construtor vazio.
+ */
 Utilizador::Utilizador()
 {
 	Utilizador(0, "", 0, "", "");
 }
 
+
 /**
-*Construtor completo
-*@param codUtilizador codigo do utilizador
-*@param nome nome do utilizador
-*@param telefone telefone do utilizador
-*@param login username do utilizador
-*@param pass password do utilizdor
-*/
+ * Construtor completo
+ * @param 	codUtilizador 	codigo do utilizador
+ * @param 	nome 			nome do utilizador
+ * @param 	telefone 		telefone do utilizador
+ * @param 	login 			username do utilizador na aplicacao
+ * @param 	pass 			password do utilizdor na aplicacao
+ */
 Utilizador::Utilizador(int codUtilizador, string nome, int telefone, string login, string pass)
 {
 	setCodUtilizador(codUtilizador);
@@ -62,124 +64,138 @@ Utilizador::Utilizador(int codUtilizador, string nome, int telefone, string logi
 	setPass(pass);
 }
 
+
 /**
-*Construtor copia
-*@param u utilizador a ser copiado
-*/Utilizador::Utilizador(const Utilizador &u)
+ * Construtor copia
+ * @param 	u 	objecto utilizador a ser copiado
+ */
+Utilizador::Utilizador(const Utilizador &u)
 {
 	Utilizador(u.codUtilizador, u.nome, u.telefone, u.login, u.pass);
 }
 
+
 /**
-*Destrutor
-*/
+ * Destrutor
+ */
 Utilizador::~Utilizador()
 {
 }
 
+
 /**
-*Metodo para retornar o codigo de utilizador
-*@return codigo do utilizador
-*/
+ * Metodo para retornar o codigo de utilizador
+ * @return 	devolve codigo do utilizador
+ */
 int Utilizador::getCodUtilizador() const
 {
 	return codUtilizador;
 }
 
+
 /**
-*Metodo para retornar o nome
-*@return nome do utilizador
-*/
+ * Metodo para retornar o nome
+ * @return 	devolve nome do utilizador
+ */
 string Utilizador::getNome() const
 {
 	return nome;
 }
 
+
 /**
-*Metodo para retornar telefone
-*@return telefone do utilizador
-*/
+ * Metodo para retornar telefone
+ * @return 	devolve numero de telefone do utilizador
+ */
 int Utilizador::getTelefone() const
 {
 	return telefone;
 }
 
+
 /**
-*Metodo para retornar nome de login
-*@return nome de login 
-*/
+ * Metodo para retornar nome de login
+ * @return 	devolce o login do utilizador
+ */
 string Utilizador::getLogin() const
 {
 	return login;
 }
 
+
 /**
-*Metodo para retornar password do utilizador
-*@return password do utilizador
-*/
+ * Metodo para retornar password do utilizador
+ * @return 	devolve a password do utilizador
+ */
 string Utilizador::getPass() const
 {
 	return pass;
 }
 
+
 /**
-*Metodo para alterar codigo de utilizador
-*@param codigo de utilizador para alterar
-*/
+ * Metodo para alterar codigo de utilizador
+ * @param 	codigo de utilizador a alterar
+ */
 void Utilizador::setCodUtilizador(const int c)
 {
 	codUtilizador = c;
 }
 
+
 /**
-*Metodo para alterar o nome do utilizador
-*@param nome para alterar
-*/
+ * Metodo para alterar o nome do utilizador
+ * @param 	nome a alterar
+ */
 void Utilizador::setNome(const string n)
 {
 	nome = n;
 }
 
 /**
-*Metodo para alterar o telefone do utilizador
-*@param telefone para alterar
-*/
+ * Metodo para alterar o telefone do utilizador
+ * @param 	telefone a alterar
+ */
 void Utilizador::setTelefone(const int t)
 {
 	telefone = t;
 }
 
+
 /**
-*Metodo para alterar o nome de login do utilizador
-*@param nome para alterar
-*/
+ * Metodo para alterar o nome de login do utilizador
+ * @param 	nome a alterar
+ */
 void Utilizador::setLogin(const string l)
 {
 	login = l;
 }
 
+
 /**
-*Metodo para alterar a password do utilizador
-*@param nova password para ser alterada
-*/
+ * Metodo para alterar a password do utilizador
+ * @param 	nova password para a ser alterada
+ */
 void Utilizador::setPass(const string p)
 {
 	pass = p;
 }
 
+
 /**
-*Metodo para escrever todos os atributos do objeto Utilizador
-*@param out objeto stream out
-*/
+ * Metodo para escrever todos os atributos do objeto Utilizador
+ * @param 	out 	objeto de stream out
+ */
 void Utilizador::escreve(ostream &out) const {
 	out << setw(5) << codUtilizador << " | " << nome << " | " << telefone << " | " << login << " | " << pass << endl;
 }
 
+
 /**
-*Sobrecarga do operador stream out
-*@param out objet stream out
-*@param c objeto objeto Utilizador
-*/
+ * Sobrecarga do operador stream out
+ * @param 	out 	objeto de stream out
+ * @param 	c 		objeto do tipo Utilizador
+ */
 ostream & operator << (ostream &out, const Utilizador &c) {
 	c.escreve(out);
 	return out;
