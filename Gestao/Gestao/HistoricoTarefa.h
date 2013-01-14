02d90ff1af7e701d4e14b234c4c4b9aadba538a5
Vitor Hugo <vitorhugoanjosowns@gmail.com>
2013-01-14 21:21:12 +0000

    Documentacao

--- Gestao/Gestao/HistoricoTarefa.h
+++ Gestao/Gestao/HistoricoTarefa.h
@@ -3,7 +3,8 @@
 *@author Vitor Hugo
 *@date 14/01/2013 
 *@file HistoricoTarefa.h 
-*Classe responsavel por guardar todo o historico das tarefas.*/ 
+*Classe responsavel por guardar todo o historico das tarefas
+*/ 
 
 #ifndef HistoricoTarefa_H
 #define HistoricoTarefa_H
@@ -100,6 +101,25 @@ HistoricoTarefa::HistoricoTarefa()
 
 /**
  *Construtor completo
+ *@param codhistorico 		codigo de historico
+ *@param codtarefa 			codigo de tarefa
+ *@param codprojecto 		codigo de projecto
+ *@param codestado 			codigo de estado
+ *@param nivelimportancia 	nivel de importancia
+ *@param datacriacao		data de criacao
+ *@param datafim 			data de conclusao
+ *@param informacao 		informacao
+ *@param dataestimada 		data estimada
+ *@param duracao 			duracao
+ *@param tipo 				tipo
+ *@param titulo 			titulo
+ *@param dependente 		dependente
+ *@param codutilizador 		codigo de utilizador
+ *@param ncontexto  		numero de contexto
+ *@param delegado 			delegado
+ *@param datainsercao 		data de insercao
+ *@param codtipo 			codigo do tipo
+ *@param info 				informacao
  */
 HistoricoTarefa::HistoricoTarefa(int codhistorico, int codtarefa, int codprojecto, int codestado, int nivelimportancia, Data datacriacao, Data datafim, string informacao, Data dataestimada, int duracao, string tipo, string titulo, int dependente, int codutilizador, int ncontexto, int delegado, Data datainsercao, int codtipo, string info)
 {
@@ -127,7 +147,7 @@ HistoricoTarefa::HistoricoTarefa(int codhistorico, int codtarefa, int codproject
 
 /**
  *Construtor copia
- * @param objeto hi do tipo HistoricoTarefa a ser copiado
+ * @param objeto ht do tipo HistoricoTarefa a ser copiado
  */
 HistoricoTarefa::HistoricoTarefa(const HistoricoTarefa& ht)
 {
@@ -159,181 +179,354 @@ HistoricoTarefa::HistoricoTarefa(const HistoricoTarefa& ht)
 HistoricoTarefa::~HistoricoTarefa()
 {
 }
+
 /**
- * @return codigo do historico
+ * Metodo para retornar o codigo de historico.
+ * @return devolve o codigo de historico.
  */
 int HistoricoTarefa::getCodHistorico()const
 {
  return codHistorico;
 }
-int HistoricoTarefa::getCodTarefa() const
+i
+/**
+ * Metodo para retornar o codigo de tarefa.
+ * @return devolve o codigo de tarefa.
+ */nt HistoricoTarefa::getCodTarefa() const
 {
  return codTarefa;
 }
-int HistoricoTarefa::getCodProjecto() const
+i
+/**
+ * Metodo para retornar o codigo de projecto.
+ * @return devolve o codigo de projecto.
+ */nt HistoricoTarefa::getCodProjecto() const
 {
  return codProjecto;
 }
 
+/**
+ * Metodo para retornar o codigo de estado.
+ * @return devolve o codigo de estado.
+ */
 int HistoricoTarefa::getCodEstado() const
 {
  return codEstado;
 }
 
+/**
+ * Metodo para retornar o nivel de importancia.
+ * @return devolve o nivel de importancia.
+ */
 int HistoricoTarefa::getNivelImportancia() const
 {
  return nivelImportancia;
 }
-Data HistoricoTarefa::getDataInicio()
+D
+/**
+ * Metodo para retornar a data de inicio.
+ * @return devolve a data de inicio.
+ */ata HistoricoTarefa::getDataInicio()
 {
  return dataInicio;
 }
 
+/**
+ * Metodo para retornar a data de conclusao.
+ * @return devolve a data de conclusao.
+ */
 Data HistoricoTarefa::getDataFim()
 {
  return dataFim;
 }
 
+/**
+ * Metodo para retornar a data estimada.
+ * @return devolve a data estimada.
+ */
 Data HistoricoTarefa::getDataEstimada()
 {
  return dataEstimada;
 }
 
+/**
+ * Metodo para retornar a data de insercao.
+ * @return devolve a data de insercao.
+ */
 Data HistoricoTarefa::getDataInsercao()
 {
  return dataInsercao;
 }
 
+/**
+ * Metodo para retornar a informacao.
+ * @return devolve a informacao.
+ */
 string HistoricoTarefa::getInformacao() const
 {
  return informacao;
 }
 
+/**
+ * Metodo para retornar a duracao.
+ * @return devolve a duracao.
+ */
 int HistoricoTarefa::getDuracao() const 
 {
 	return duracao;
 }
 
+/**
+ * Metodo para retornar o tipo.
+ * @return devolve o tipo.
+ */
 string HistoricoTarefa::getTipo() const 
 {
 	return tipo;
 }
 
+/**
+ * Metodo para retornar o titulo.
+ * @return devolve o titulo.
+ */
 string HistoricoTarefa::getTitulo() const 
 {
 	return titulo;
 }
 
+/**
+ * Metodo para retornar a tarefa dependente.
+ * @return devolve a tarefa dependente.
+ */
 int HistoricoTarefa::getDependente() const 
 {
 	return dependente;
 }
 
+/**
+ * Metodo para retornar o codigo de utilizador.
+ * @return devolve o codigo de utilizador.
+ */
 int HistoricoTarefa::getCodUtilizador() const 
 {
 	return codUtilizador;
 }
 
+/**
+ * Metodo para retornar o numero de contexto.
+ * @return devolve o numero de contexto.
+ */
 int HistoricoTarefa::getNContexto() const 
 {
 	return nContexto;
 }
 
+/**
+ * Metodo para retornar o delegado.
+ * @return devolve o delegado.
+ */
 int HistoricoTarefa::getDelegado() const 
 {
 	return delegado;
 }
 
+/**
+ * Metodo para retornar o codigo de tipo.
+ * @return devolve o codigo de tipo.
+ */
 int HistoricoTarefa::getCodTipo() const 
 {
 	return codTipo;
 }
 
+/**
+ * Metodo para retornar a informacao.
+ * @return devolve a informacao.
+ */
 string HistoricoTarefa::getInfo() const 
 {
 	return info;
 }
 
- void HistoricoTarefa::setCodHistorico(const int codhistorico)
- {
- 	codHistorico = codhistorico;
- }
- void HistoricoTarefa::setCodTarefa(const int codtarefa) 
- {
- 	codTarefa = codtarefa;
- }
- void HistoricoTarefa::setCodProjecto(const int codprojecto) 
- {
- 	codProjecto = codprojecto;
- }
- void HistoricoTarefa::setCodEstado(const int codestado) 
- {
- 	codEstado = codestado;
- }
- void HistoricoTarefa::setCodUtilizador(const int codutilizador) 
- {
- 	codUtilizador = codutilizador;
- }
- void HistoricoTarefa::setNivelImportancia(const int nivelimportancia) 
- {
- 	nivelImportancia = nivelimportancia;
- }
- void HistoricoTarefa::setDuracao(const int duracao) 
- {
- 	this->duracao = duracao;
- }
- void HistoricoTarefa::setDependente(const int dependente)
- {
- 	this->dependente = dependente;
- }
- void HistoricoTarefa::setNContexto(const int ncontexto)
- {
- 	nContexto = ncontexto;
- }
- void HistoricoTarefa::setDelegado(const int delegado)
- {
- 	this->delegado = delegado;
- }
- void HistoricoTarefa::setCodTipo(const int codtipo)
- {
- 	codTipo = codtipo;
- }
- void HistoricoTarefa::setDataInicio(const Data dataInicio)
- {
- 	this->dataInicio = dataInicio;
- }
- void HistoricoTarefa::setDataFim(const Data datafim)
- {
- 	dataFim = datafim;
- }
- void HistoricoTarefa::setDataEstimada(const Data dataestimada)
- {
- 	dataEstimada = dataestimada;
- }
- void HistoricoTarefa::setDataInsercao(const Data datainsercao)
- {
- 	dataInsercao = datainsercao;
- }
- void HistoricoTarefa::setInformacao(const string informacao) 
- {
- 	this->informacao = informacao;
- }
- void HistoricoTarefa::setInfo(const string info) 
- {
- 	this->info = info;
- }
- void HistoricoTarefa::setDescricao(const string descricao) 
- {
- 	this->descricao = descricao;
- }
- void HistoricoTarefa::setTipo(const string tipo)
- {
- 	this->tipo = tipo;
- }
- void HistoricoTarefa::setTitulo(const string titulo)
- {
- 	this->titulo = titulo;
- }
+/**
+ * Metodo de atribuicao do codigo de historico.
+ * @param codhistorico codigo de historico.
+ */
+void HistoricoTarefa::setCodHistorico(const int codhistorico)
+{
+	codHistorico = codhistorico;
+}
+
+/**
+ * Metodo de atribuicao do codigo de tarefa.
+ * @param codtarefa codigo de tarefa.
+ */
+void HistoricoTarefa::setCodTarefa(const int codtarefa) 
+{
+	codTarefa = codtarefa;
+}
+
+/**
+ * Metodo de atribuicao do codigo de projecto.
+ * @param codprojecto codigo de projecto.
+ */
+void HistoricoTarefa::setCodProjecto(const int codprojecto) 
+{
+	codProjecto = codprojecto;
+}
+
+/**
+ * Metodo de atribuicao do codigo de estado.
+ * @param codestado codigo de estado.
+ */
+void HistoricoTarefa::setCodEstado(const int codestado) 
+{
+	codEstado = codestado;
+}
+
+/**
+ * Metodo de atribuicao do codigo de utilizador.
+ * @param codutilizador codigo de utilizador.
+ */
+void HistoricoTarefa::setCodUtilizador(const int codutilizador) 
+{
+	codUtilizador = codutilizador;
+}
+
+/**
+ * Metodo de atribuicao do nivel de importancia.
+ * @param nivelimportancia nivel de importancia.
+ */
+void HistoricoTarefa::setNivelImportancia(const int nivelimportancia) 
+{
+	nivelImportancia = nivelimportancia;
+}
+
+/**
+ * Metodo de atribuicao da duracao.
+ * @param duracao duracao.
+ */
+void HistoricoTarefa::setDuracao(const int duracao) 
+{
+	this->duracao = duracao;
+}
+
+/**
+ * Metodo de atribuicao da tarefa dependente.
+ * @param dependente tarefa dependente.
+ */
+void HistoricoTarefa::setDependente(const int dependente)
+{
+	this->dependente = dependente;
+}
+
+/**
+ * Metodo de atribuicao do numero de contexto.
+ * @param ncontexto numero de contexto.
+ */
+void HistoricoTarefa::setNContexto(const int ncontexto)
+{
+	nContexto = ncontexto;
+}
+
+/**
+ * Metodo de atribuicao do delegado.
+ * @param delegado delegado.
+ */
+void HistoricoTarefa::setDelegado(const int delegado)
+{
+	this->delegado = delegado;
+}
+
+/**
+ * Metodo de atribuicao do codigo de tipo.
+ * @param codtipo codigo de tipo.
+ */
+void HistoricoTarefa::setCodTipo(const int codtipo)
+{
+	codTipo = codtipo;
+}
+
+/**
+ * Metodo de atribuicao da data de inicio.
+ * @param dataInicio data de inicio.
+ */
+void HistoricoTarefa::setDataInicio(const Data dataInicio)
+{
+	this->dataInicio = dataInicio;
+}
+
+/**
+ * Metodo de atribuicao da data de conclusao.
+ * @param datafim data de conclusao.
+ */
+void HistoricoTarefa::setDataFim(const Data datafim)
+{
+	dataFim = datafim;
+}
+
+/**
+ * Metodo de atribuicao da data estimada.
+ * @param dataestimada data estimada.
+ */
+void HistoricoTarefa::setDataEstimada(const Data dataestimada)
+{
+	dataEstimada = dataestimada;
+}
+
+/**
+ * Metodo de atribuicao da data de insercao.
+ * @param datainsercao data de insercao.
+ */
+void HistoricoTarefa::setDataInsercao(const Data datainsercao)
+{
+	dataInsercao = datainsercao;
+}
+
+/**
+ * Metodo de atribuicao da informacao.
+ * @param informacao informacao.
+ */
+void HistoricoTarefa::setInformacao(const string informacao) 
+{
+	this->informacao = informacao;
+}
+
+/**
+ * Metodo de atribuicao da info.
+ * @param codinformacao codigo de informacao.
+ */
+void HistoricoTarefa::setInfo(const string info) 
+{
+	this->info = info;
+}
+
+/**
+ * Metodo de atribuicao da descricao.
+ * @param descricao descricao.
+ */
+void HistoricoTarefa::setDescricao(const string descricao) 
+{
+	this->descricao = descricao;
+}
+
+/**
+ * Metodo de atribuicao do tipo.
+ * @param tipo tipo.
+ */
+void HistoricoTarefa::setTipo(const string tipo)
+{
+	this->tipo = tipo;
+}
+
+/**
+ * Metodo de atribuicao do titulo.
+ * @param titulo titulo.
+ */
+void HistoricoTarefa::setTitulo(const string titulo)
+{
+	this->titulo = titulo;
+}
 
 
 /**
@@ -374,6 +567,10 @@ ostream & operator << (ostream &out, const HistoricoTarefa &ht)
 	return out;
 }
 
+/**
+ * Clone
+ * @return devolve um clone.
+ */
 HistoricoTarefa* HistoricoTarefa::clone() const
 {
 	return new HistoricoTarefa(*this);
